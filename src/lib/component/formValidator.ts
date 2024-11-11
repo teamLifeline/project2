70d06602440e0add064d73afc25e0c8f81a0efc0
Chris Jen Ian D. Roa <chrisianroa@gmail.com>
2024-11-11 15:50:32 +0800

    debug

--- src/lib/component/formValidator.ts
+++ src/lib/component/formValidator.ts
@@ -11,34 +11,33 @@ export function validateForm(
   email: string,
   phoneNumber: string,
   address: string,
-  reason: string
+  reason: string,
+  isShopForm: boolean = false
 ): ValidationErrors {
   const errors: ValidationErrors = {};
 
   if (!name.trim()) {
-      errors.name = "Name is required.";
+    errors.name = "Name is required.";
   }
 
   if (!email.trim()) {
-      errors.email = "Email is required.";
+    errors.email = "Email is required.";
   } else if (!/\S+@\S+\.\S+/.test(email)) {
-      errors.email = "Email is invalid.";
+    errors.email = "Email is invalid.";
   }
 
   if (!phoneNumber.trim()) {
-      errors.phoneNumber = "Phone number is required.";
+    errors.phoneNumber = "Phone number is required.";
   } else if (!/^\d{10}$/.test(phoneNumber.replace(/\D/g, ""))) {
-      errors.phoneNumber = "Phone number must be exactly 10 digits long.";
+    errors.phoneNumber = "Phone number must be exactly 10 digits long.";
   }
 
   if (!address.trim()) {
-    errors.phoneNumber = "address is required.";
-} else if (!/^\d{10}$/.test(phoneNumber.replace(/\D/g, ""))) {
-    errors.phoneNumber = "Address is required.";
-}
+    errors.address = "Address is required.";
+  }
 
-  if (!reason.trim()) {
-      errors.reason = "Reason for contact is required.";
+  if (!isShopForm && !reason.trim()) {
+    errors.reason = "Reason for contact is required.";
   }
 
   return errors;
